eba93e282edcbdda7f6bfa480e5c8f800ee1c6f8
Andrew Lodes <alodes999@gmail.com>
2015-07-17 12:43:20 -0500

    Add basic view structure album/photo table

---
+++ controllers/albumphoto_controller.rb
@@ -0,0 +1,19 @@
+get "/albumphoto-main" do
+  erb :"/main/albumphoto-main"
+end
+
+get "/albumphoto-view" do
+  erb :"/albumphoto/view"
+end
+
+get "/albumphoto-add" do
+  erb :"/albumphoto/add"
+end
+
+get "/albumphoto-edit" do
+  erb :"/albumphoto/edit"
+end
+
+get "/albumphoto-delete" do
+  erb :"/albumphoto/delete"
+end
