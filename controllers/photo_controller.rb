9ffc88ac221a339117b487245870f7f502096250
Andrew Lodes <alodes999@gmail.com>
2015-07-17 16:16:58 -0500

    Add controller functionality for photos

--- controllers/photo_controller.rb
+++ controllers/photo_controller.rb
@@ -3,6 +3,7 @@ get "/photo-main" do
 end
 
 get "/photo-view" do
+  @photo = Photo.all
   erb :"/photo/view"
 end
 
@@ -10,10 +11,40 @@ get "/photo-add" do
   erb :"/photo/add"
 end
 
+get "/photo-to-database" do
+  add_hash = {"name": "#{params["photographer"]["name"]}", "cameratype": "#{params["photographer"]["cameratype"]}", "employer": "#{params["photographer"]["employer"]}"}
+  
+  a = Photo.add(add_hash)
+  
+  if a.save
+    erb :"/success/success"
+  else
+    @errors = a.errors.messages
+  end
+  
+end
+ 
 get "/photo-edit" do
+  @photo = Photo.all
   erb :"/photo/edit"
 end
 
+get "/edit-photo-in-database" do
+  photo_to_edit = Photo.find(params["photo"]["id"])
+  
+  photo_to_edit.title = params["photo"]["title"]
+  photo_to_edit.photographer_id = params["photo"]["photographer_id"]
+  
+  if photo_to_edit.save
+    erb :"/success/success"
+  else
+    @errors = photo_to_edit.errors.messages
+    erb :"/failure/failure"
+  end
+  
+end
+
 get "/photo-delete" do
+  @photo = Photo.all
   erb :"/photo/delete"
 end
