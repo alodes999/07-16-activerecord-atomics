e18cbfd7a8efdab353c358de71b782ea7fa6a313
Andrew Lodes <alodes999@gmail.com>
2015-07-17 16:07:52 -0500

    Add CRUD action to photographer views
    Add copy/pasted code to other views, awaiting changes

--- controllers/photographer_controller.rb
+++ controllers/photographer_controller.rb
@@ -11,10 +11,40 @@ get "/photographer-add" do
   erb :"/photographer/add"  
 end
 
+get "/photographer_to_database" do
+  add_hash = {"name": "#{params["photographer"]["name"]}", "cameratype": "#{params["photographer"]["cameratype"]}", "employer": "#{params["photographer"]["employer"]}"}
+  
+  Photographer.create(add_hash)
+  erb :"/success/success"
+end
+
 get "/photographer-edit" do
+  @photographer = Photographer.all
   erb :"/photographer/edit"  
 end
 
+get "/edit_photographer_to_database" do
+  photog_to_edit = Photographer.find(params["photographer"]["id"])
+  
+  photog_to_edit.name = params["photographer"]["name"]
+  photog_to_edit.cameratype = params["photographer"]["cameratype"]
+  photog_to_edit.employer = params["photographer"]["employer"]
+  
+  if photog_to_edit.save
+    erb :"/success/success"
+  else
+    @errors = photog_to_edit.errors.messages
+    erb :"/failure/failure"
+  end
+  
+end
+
 get "/photographer-delete" do
+  @photographer = Photographer.all
   erb :"/photographer/delete"  
+end
+
+get "/photographer-delete-action" do
+  Photographer.delete(params["photographer"]["id"])
+  erb :"/success/success"
 end
--- models/album.rb
+++ models/album.rb
@@ -1,6 +1,6 @@
 class Album < ActiveRecord::Base
   has_and_belongs_to_many :photos
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   unless ActiveRecord::Base.connection.table_exists?(:albums)
     ActiveRecord::Base.connection.create_table :albums do |p|
       p.string :title
