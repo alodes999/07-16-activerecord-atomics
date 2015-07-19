14f49dece4d9317f74528eb6ce52bd51e9f64490
Andrew Lodes <alodes999@gmail.com>
2015-07-19 16:47:34 -0500

    Change app for heroku deployment

--- app.rb
+++ app.rb
@@ -3,16 +3,29 @@ require "bundler/setup"
 
 require "pry"
 require "active_record"
-require "sqlite3"
 require "bcrypt"
 require "sinatra"
 require "sinatra/reloader"
 require "sinatra/cross_origin"
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'your_database.db')
+configure :development do
+  require 'sqlite3'
+  ActiveRecord:Base.establish_connection(adapter: 'sqlite3', database: 'your_database.db')
+end
 
-# So that ActiveRecord explains the SQL it's running in the logs.
-ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
+configure :production do  
+  require 'pg'
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
 
 # Models
 require_relative "models/photographer.rb"
