bb9ba7f4ba8f5165b9132cfaa2ffe72ab1982de2
Andrew Lodes <alodes999@gmail.com>
2015-07-19 16:44:37 -0500

    Finish delete views for bridge
    Add config.ru

--- controllers/albumphoto_controller.rb
+++ controllers/albumphoto_controller.rb
@@ -44,5 +44,22 @@ get "/albumphoto-view-top-photos" do
 end
 
 get "/albumphoto-delete" do
+  @photo = Photo.all
   erb :"/albumphoto/delete"
+end
+
+get "/albumphoto-delete-step2" do
+  @photo = Photo.find(params["photo"]["id"])
+  @albumlist = @photo.albums
+  
+  erb :"/albumphoto/delstep2"
+end
+
+get "/albumphoto-delete-action" do
+  photo_to_del = Photo.find(params["album"]["photo_id"])
+  album_to_remove = Album.find(params["album"]["id"])
+  
+  photo_to_del.albums.delete(album_to_remove)
+  
+  erb :"/success/success"
 end
